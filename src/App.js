b08a4d124e0c6ea7b3c97bbb784355922831f946
Dominic Cummings <dominiccummings95@gmail.com>
2023-09-29 10:10:29 -0500

    Sample data for movie

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useEffect } from 'react'
+import './App.css';
+import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=6882f385';
 
@@ -10,13 +12,48 @@ const App = () => {
 
     console.log(data.Search);
   }
-  
+
   useEffect(() => {
     searchMovies('Avengers');
   }, [] );
+  
+  const movie1 = {
+    "Title": "The Avengers",
+    "Year": "2012",
+    "imdbID": "tt0848228",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+}
   return (
-    <div>
-      <h1>App</h1>
+    <div className='app'>
+      <h1>Movie Search</h1>
+      <div className='search'>
+        <input 
+          placeholder='Search for movies'
+          value='Superman'
+          onChange={() => {}}
+        />
+        <img 
+          src={SearchIcon} 
+          alt='Search' 
+          onClick={() => {}} 
+        />
+      </div>
+      <div className='container'>
+        <div className='movie'>
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+          </div>
+
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
