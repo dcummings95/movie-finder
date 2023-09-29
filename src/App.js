8a53db98c53c0f0f5ca64129597317d93b29aec5
Dominic Cummings <dominiccummings95@gmail.com>
2023-09-29 10:18:12 -0500

    Movies all displayed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
@@ -6,26 +6,19 @@ import MovieCard from './MovieCard';
 const API_URL = 'http://www.omdbapi.com?apikey=6882f385';
 
 const App = () => {
-  
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   }
 
   useEffect(() => {
     searchMovies('Avengers');
   }, [] );
   
-  const movie = {
-    "Title": "The Avengers",
-    "Year": "2012",
-    "imdbID": "tt0848228",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
-}
   return (
     <div className='app'>
       <h1>Movie Search</h1>
@@ -42,9 +35,18 @@ const App = () => {
         />
       </div>
 
-      <div className='container'>
-        <MovieCard movie={movie}/>
-      </div>
+      {movies?.length > 0
+        ? (
+            <div className='container'>
+              {movies.map((movie) => (
+                  <MovieCard movie={movie}/>
+              ))}
+            </div>
+          ) : (
+            <div className='empty'>
+              <h2>No movies found</h2>
+            </div>
+          )}
     </div>
   )
 }
