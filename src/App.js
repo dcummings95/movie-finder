34d8d59ec2fc5b51dbbc0b212a84698d8a7b9e89
Dominic Cummings <dominiccummings95@gmail.com>
2023-09-29 09:55:21 -0500

    Testing api

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ const API_URL = 'http://www.omdbapi.com?apikey=6882f385';
 const App = () => {
 
   const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
 
+    console.log(data.Search);
   }
+  
   useEffect(() => {
-    
+    searchMovies('Avengers');
   }, [] );
   return (
     <div>
